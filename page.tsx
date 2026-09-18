97bd70cde13d05bcb00d7d597856b433ee861c89
musickamiloo-arch <musickamiloo@gmail.com>
2026-09-18 13:57:52 +0200

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <h1>🐱⚡ MOVO Namysłów</h1>
+      <p>Aplikacja została pomyślnie uruchomiona!</p>
+    </main>
+  );
+}
